585b98a13ab949b4b467d45b541567c0b9c0d90c
SodaCris <wang-jiaxun@outlook.com>
2023-05-03 21:50:50 +0800

    version 1: by observation

--- build.ps1
+++ build.ps1
@@ -1,3 +1,4 @@
-Remove-Item -Recurse -Force .\build
-cmake -Bbuild -GNinja -DCMAKE_BUILD_TYPE=Debug
+# Remove-Item -Recurse -Force .\build
+# cmake -Bbuild -GNinja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=true
 cmake --build build
+# Copy-Item .\build\compile_commands.json .
--- problem_3_6.cpp
+++ problem_3_6.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <cstddef>
 #include <fstream>
 #include <functional>
 #include <ios>
@@ -13,36 +14,93 @@ template <typename T> struct bintree {
     bintree *right{nullptr};
     T data;
 
+    bintree *successor{nullptr};
+    bintree *predecessor{nullptr};
+
     bintree<T>() {}
     bintree<T>(T data) { this->data = data; }
 };
 
-template <typename T> void insert_tree(bintree<T> *&tree, T data);
+enum class Direction { Left, Right };
+
+template <typename T> struct insertion_path {
+    std::vector<Direction> directions;
+    std::vector<bintree<T> *> pointers;
+};
+
+template <typename T>
+void insert_tree(bintree<T> *&tree, T data, insertion_path<T> &path);
 
 template <typename T> bintree<T> *create_tree(std::vector<T> data) {
     if (data.size() == 0)
         return nullptr;
-    auto head = new bintree<T>();
-    head->data = data.at(0);
+    auto head = new bintree<T>(data.at(0));
 
     for (auto i = 1; i < data.size(); i++) {
-        if (data.at(i) < head->data) {
-            insert_tree(head->left, data.at(i));
-        } else if (data.at(i) > head->data) {
-            insert_tree(head->right, data.at(i));
-        }
+        auto path = insertion_path<T>();
+        insert_tree(head, data.at(i), path);
     }
     return head;
 }
 
-template <typename T> void insert_tree(bintree<T> *&tree, T data) {
-    if (tree == nullptr)
+template <typename T, Direction D1, Direction D2>
+bintree<T> *find_path(insertion_path<T> path) {
+    if (path.pointers.size() == 0)
+        return nullptr;
+    if (path.directions.at(path.directions.size() - 1) == D1) {
+        return path.pointers.at(path.pointers.size() - 1);
+    } else {
+        while (path.directions.at(path.directions.size() - 1) == D2) {
+            path.directions.pop_back();
+            path.pointers.pop_back();
+            if (path.directions.size() == 0)
+                return nullptr;
+        }
+        return path.pointers.at(path.pointers.size() - 1);
+    }
+}
+
+template <typename T> bintree<T> *get_successor(insertion_path<T> path) {
+    return find_path<T, Direction::Left, Direction::Right>(path);
+}
+
+template <typename T> bintree<T> *get_predecessor(insertion_path<T> path) {
+    return find_path<T, Direction::Right, Direction::Left>(path);
+}
+
+template <typename T>
+void insert_tree(bintree<T> *&tree, T data, insertion_path<T> &path) {
+    if (tree == nullptr) {
         tree = new bintree<T>(data);
-    else {
+        auto successor = get_successor(path);
+        if (successor) {
+            auto previous_predecessor = successor->predecessor;
+            successor->predecessor = tree;
+            tree->successor = successor;
+            if (previous_predecessor) {
+                previous_predecessor->successor = tree;
+                tree->predecessor = previous_predecessor;
+            }
+        } else {
+            auto predecessor = get_predecessor(path);
+            if (predecessor) {
+                auto previous_successor = predecessor->successor;
+                predecessor->successor = tree;
+                tree->predecessor = predecessor;
+                if (previous_successor) {
+                    previous_successor->predecessor = tree;
+                    tree->successor = previous_successor;
+                }
+            }
+        }
+    } else {
+        path.pointers.push_back(tree);
         if (data < tree->data) {
-            insert_tree(tree->left, data);
+            path.directions.push_back(Direction::Left);
+            insert_tree(tree->left, data, path);
         } else if (data > tree->data) {
-            insert_tree(tree->right, data);
+            path.directions.push_back(Direction::Right);
+            insert_tree(tree->right, data, path);
         }
     }
 }
@@ -71,14 +129,28 @@ template <typename T> std::vector<T> in_order_data(bintree<T> *tree) {
     return data_;
 }
 
+template <typename T> std::vector<T> traverse_successor(bintree<T> *tree) {
+    auto data = std::vector<T>();
+    if (tree != nullptr) {
+        while (tree->predecessor != nullptr) {
+            tree = tree->predecessor;
+        }
+    }
+    while (tree != nullptr) {
+        data.push_back(tree->data);
+        tree = tree->successor;
+    }
+    return data;
+}
+
 std::vector<int> read_data() {
     auto in = std::ifstream("data.txt", std::ios_base::in);
     auto line = std::string();
     auto data = std::vector<int>();
-    while(std::getline(in, line)) {
+    while (std::getline(in, line)) {
         auto sstream = std::stringstream(line);
         auto number = int{0};
-        while(sstream >> number) {
+        while (sstream >> number) {
             data.push_back(number);
         }
     }
@@ -94,6 +166,10 @@ void write_file(std::vector<int> data) {
 
 int main() {
     auto numbers = read_data();
+    for (auto i : numbers) {
+        std::cout << i << ' ';
+    }
+    std::cout << '\n';
     auto tree = create_tree(numbers);
     in_order_print(tree);
     auto data = in_order_data(tree);
@@ -101,4 +177,14 @@ int main() {
         std::cout << "sorted\n";
     }
     write_file(data);
+    auto data_successor_traverse = traverse_successor(tree);
+    for (auto i : data_successor_traverse) {
+        std::cout << i << ' ';
+    }
+    std::cout << '\n';
+    if (data == data_successor_traverse) {
+        std::cout << "equal\n";
+    } else {
+        std::cout << "not equal\n";
+    }
 }
